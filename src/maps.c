e6c7c150e466322222a1dbd1694a35996a514c98
Enzosbx <91389519+Enzosbx@users.noreply.github.com>
2021-10-06 18:37:11 -0300

    Update maps.c

--- src/maps.c
+++ src/maps.c
@@ -1,1 +1,25 @@
 #include "maps.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+Map loadMap(char* filename) {
+   
+    printf(filename);
+    Map newMap;
+    newMap.height = 10;
+    // Ler arquivo
+
+    FILE *arq;
+    int result;
+    char Str[50];
+    arq = fopen (filename, "rt");
+    if (arq == NULL) 
+    {
+      printf ("Problemas na criacao do arquivo \n");
+      return;
+    }
+    // Fim da leitura
+
+    return newMap;
+}
+
