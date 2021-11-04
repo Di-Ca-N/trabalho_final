681ee27616e2a7ee9c2e53d629ed2951db120817
Diego Cardoso Nunes <diegocar.nunes@gmail.com>
2021-11-04 20:15:59 -0300

    Removed non used menu

--- src/menus.c
+++ src/menus.c
@@ -21,16 +21,6 @@ Menu getMenu(MenuType type) {
         };
         return menu;
     }
-    
-    case MENU_GAME_OVER: {
-        Menu menu = {
-            .options = {"Menu principal", "Jogar novamente"},
-            .numOptions = 2,
-            .selectedOption = 0,
-            .selectionDone = false,
-        };
-        return menu;
-    }
 
     case MENU_YES_NO: {
         Menu menu = {
